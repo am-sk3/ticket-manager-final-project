f65a51a986b6a49999aa2e950db719eb29788ec3
am-sk3 <extreme.sk3@gmail.com>
2020-03-10 20:28:10 +0000

    removed unused code

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,7 +2,6 @@
 import { Request, Response } from 'express';
 import UsersRepository from '../repositories/users.repository';
 import User from '../models/User';
-import { cat } from 'shelljs';
 
 class UsersController {
     public async getAll(req: Request, res: Response): Promise<Response> {
