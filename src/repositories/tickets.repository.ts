9c1e34ad65f2e64512831cbc03d17bfffe82dc2a
am-sk3 <extreme.sk3@gmail.com>
2020-03-12 21:41:23 +0000

    fix closedDate

--- src/repositories/tickets.repository.ts
+++ src/repositories/tickets.repository.ts
@@ -5,9 +5,10 @@ import Ticket from '../models/Ticket';
 import { DateTime } from 'mssql';
 
 export default class TicketsRepository {
-    
-    public static async getAllTickets(search?: string, page?: number): Promise<Tickets[]> {
-       
+    public static async getAllTickets(
+        search?: string,
+        page?: number
+    ): Promise<Tickets[]> {
         const query = Tickets.query().select(
             'id',
             'subject',
@@ -26,63 +27,78 @@ export default class TicketsRepository {
 
     public static async getOneTicket(ticketId: number): Promise<Tickets> {
         return Tickets.query()
-                .select(
-                    'id',
-                    'subject',
-                    'content',
-                    'status',
-                    'creation_date',
-                    'id_user',
-                    'id_company',
-                    'is_deleted',
-                    'last_update_user',
-                    'last_update'
-                )
-                .where('id', '=', ticketId)
-                .first();
+            .select(
+                'id',
+                'subject',
+                'content',
+                'status',
+                'creation_date',
+                'id_user',
+                'id_company',
+                'is_deleted',
+                'last_update_user',
+                'last_update'
+            )
+            .where('id', '=', ticketId)
+            .first();
     }
 
     public static async createTicket(ticket: Ticket): Promise<Tickets> {
-        
         return Tickets.query().insert(ticket);
     }
 
-    public static async update(ticket: Ticket, ticketId: number): Promise<Number> {
+    public static async update(
+        ticket: Ticket,
+        ticketId: number
+    ): Promise<Number> {
         console.log(ticketId);
         console.log(ticket);
         return Tickets.query()
             .findById(ticketId)
-            .patch(ticket)
+            .patch(ticket);
     }
 
-    public static async delete(ticketId: number, userId: number): Promise<Number> {
+    public static async delete(
+        ticketId: number,
+        userId: number
+    ): Promise<Number> {
         return Tickets.query()
             .findById(ticketId)
             .patch({
                 isDeleted: true
             });
-            //  how to add other updates?
+        //  how to add other updates?
     }
 
-    public static async comment(content: string, userId: number, ticketId: number): Promise<number[]> {
-        return queryBuilder.insert({
-            id_user: userId,
-            id_ticket: ticketId,
-            content
-        }).into('comments')
+    public static async comment(
+        content: string,
+        userId: number,
+        ticketId: number
+    ): Promise<number[]> {
+        return queryBuilder
+            .insert({
+                id_user: userId,
+                id_ticket: ticketId,
+                content
+            })
+            .into('comments');
         //  update tickets last_updated date and user
-        ;
     }
 
-    public static async markAsClosed(status: string, ticketId: number, userId: number): Promise<number> {
-            return Tickets.query()
+    public static async markAsClosed(
+        status: string,
+        ticketId: number,
+        userId: number
+    ): Promise<number> {
+        return Tickets.query()
             .findById(ticketId)
             .patch({
                 status: 'Closed',
-                lastUpdateUser: userId
+                lastUpdateUser: userId,
+                closedDate: moment().toDate()
             });
         // return queryBuilder('tickets')
-        //     .where({id: ticketId})    
+        //     .where({id: ticketId})
         //     .update({
         //         status: 'Closed',
         //         closed_date: moment().format('YYYY-MM-DD h:mm:ss'),
