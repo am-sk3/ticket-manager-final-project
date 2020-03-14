b0dfb0a80b26eeec6f78b9b3b34413e0009c7c4a
am-sk3 <extreme.sk3@gmail.com>
2020-03-14 18:34:18 +0000

    cleaned up permissions controller

--- src/controllers/permissions.controller.ts
+++ src/controllers/permissions.controller.ts
@@ -7,11 +7,7 @@ class AuthLevel {
         next: NextFunction
     ): Promise<any> {
         const localToken = res.locals.decodedToken;
-        // const adminPaths = '(/api/users).*|(/api/companies/).*';
-        // console.log(req.body);
-        // console.log(req.params);
-        // console.log(res.locals.decodedToken);
-        // console.log('isadmin: ', localToken.isAdmin);
+
         if (localToken.isAdmin === false) {
             if (req.params.id != localToken.user_id) {
                 return res.status(403).json({ message: 'Forbidden' });
@@ -20,16 +16,6 @@ class AuthLevel {
 
         return next();
     }
-
-    // public async verifySelfUser(req: Request, res:Response,next: NextFunction): Promise<any> {
-
-    //     const localtoken = res.locals.decodedToken
-
-    //     if (
-    //     if (req.params.id != localtoken.user_id) {
-    //         return res.status(403)
-    //     }
-    // }
 }
 
 export default new AuthLevel();
